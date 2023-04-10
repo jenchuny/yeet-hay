5a4cc668480cec2579469b6bf10f064d520e68d8
churian <jennychu95@gmail.com>
2023-04-10 17:05:24 +1000

    basic search system

--- public/index.js
+++ public/index.js
@@ -1,20 +1,21 @@
 const fruits = [
-    { fruit: 'lychee',
+    { name: 'lychee',
       heatLevel: '2'},
-    { fruit: 'banana',
+    { name: 'banana',
       heatLevel: '-2'},
-    { fruit: 'durian',
+    { name: 'durian',
       heatLevel: '1'},
-    { fruit: 'apple',
+    { name: 'apple',
       heatLevel: '0'},
-    { fruit: 'mango',
+    { name: 'mango',
       heatLevel: '2'},
-    { fruit: 'watermelon',
+    { name: 'watermelon',
       heatLevel: '-1'},
-    { fruit: 'pear',
+    { name: 'pear',
       heatlevel: '0'}
 ]
 
+const searchInput = document.querySelector('.input')
 
 searchInput.addEventListener("input", (e) => {
     // inside, we will need to achieve a few things:
@@ -26,11 +27,33 @@ searchInput.addEventListener("input", (e) => {
         // 3. redefine 'value' to exclude white space and change input to all lowercase
          value = value.trim().toLowerCase()
         // 4. return the results only if the value of the search is included in the person's name
-        // we need to write code (a function for filtering through our data to include the search input value)
+        setList(fruits.filter(fruit => {
+            return fruit.name.includes(value)
+        }))
     } else {
-        // 5. return nothing
-        // input is invalid -- show an error message or show no results
-
+        
     }
 
 })
+
+// creating and declaring a function called "setList"
+// setList takes in a param of "results"
+function setList(results){
+
+    for (const fruit of results){
+        // creating a li element for each result item
+        const resultItem = document.createElement('li')
+
+        // adding a class to each item of the results
+        resultItem.classList.add('result-item')
+
+        // grabbing the name of the current point of the loop and adding the name as the list item's text
+        const text = document.createTextNode(fruit.name)
+
+        // appending the text to the result item
+        resultItem.appendChild(text)
+
+        // appending the result item to the list
+        list.appendChild(resultItem)
+    }
+}
